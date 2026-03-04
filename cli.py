eb21fc5225dd4b89eb53432d7f229ea43a3543d4
BingYi <bwbingyi@gmail.com>
2026-03-04 11:42:44 -0600

    fixed dependencies and stuff

--- cli.py
+++ cli.py
@@ -15,7 +15,8 @@ import torch
 from PIL import Image
 
 class HuatuoChatbot():
-    def __init__(self, model_dir, device = 'cuda'):
+    #def __init__(self, model_dir, device = 'cuda'):
+    def __init__(self, model_dir, device = 'mps'):
         self.model_dir = model_dir
 
         self.gen_kwargs = {
